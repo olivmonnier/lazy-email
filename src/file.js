51ccd6485d5574fbe221c8bb2420123a125375df
olivmonnier <omonnier.dev@gmail.com>
2016-09-25 22:16:34 +0200

    Fix save method

--- src/file.js
+++ src/file.js
@@ -1,23 +1,30 @@
 const {dialog} = require('electron').remote
 const fs = require('fs');
 const inky = require('./inky');
+let FILE_OPEN = null;
 
 exports.openHtmlTemplate = function (editor) {
   dialog.showOpenDialog({ properties: ['openFile']}, function(filepath) {
     if (filepath) {
       var content = fs.readFileSync(filepath[0], 'utf8');
 
+      FILE_OPEN = filepath[0];
       editor.setValue(content);
     }
   });
 }
 
 exports.saveHtmlTemplate = function (editor) {
-  dialog.showSaveDialog(function(filepath) {
-    if (filepath) {
-      fs.writeFileSync(filepath, editor.getValue())
-    }
-  });
+  if (FILE_OPEN) {
+    fs.writeFileSync(FILE_OPEN, editor.getValue());
+  } else {
+    dialog.showSaveDialog(function(filepath) {
+      if (filepath) {
+        FILE_OPEN = filepath;
+        fs.writeFileSync(filepath, editor.getValue())
+      }
+    });
+  }
 }
 
 exports.transformHtml = function(elems) {
